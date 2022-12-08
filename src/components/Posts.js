37d900642ffd72e324b944ca7cbf847070e8f0d4
brian <kipkoechbrian0@gmail.com>
2022-12-08 12:25:03 +0300

    remove unused vars

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Button from "./Button";
 import Card from "./Card";
